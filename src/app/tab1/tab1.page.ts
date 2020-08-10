ffc7836fd2a44df130d3904cd35e3904babfe99c
Luis Rondow <luiseduardorondow@gmail.com>
2020-08-10 15:59:24 -0300

    adding blip chat widget

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BlipChat } from 'blip-chat-widget';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
-export class Tab1Page {
-
+export class Tab1Page implements OnInit {
   constructor() {}
 
+  ngOnInit() {
+    new BlipChat()
+      .withAppKey(
+        'd2hhdHNhcHBib3Q2OmQ2MGE0YWZmLTE4YWEtNDYxZi1hMDRmLWRlZmYxNDczZDAzYg=='
+      )
+      .withButton({ color: '#7895fd' })
+      .build();
+  }
 }
